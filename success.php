ce69d3d18f1bbb8583384dd27873a857dc0f7a33
Lyndsey <Lyndsey.Robinson@DFSupplyinc.com>
2024-09-14 12:09:42 -0700

    added success page

--- success.php
+++ success.php
@@ -3,25 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Message Sent</title>
+    <link rel="stylesheet" href="styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Handjet:wght@100..900&family=Lora:ital,wght@0,400..700;1,400..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <link rel="stylesheet" href="styles.css" />
-    <style>
-        .error {
-  border: 2px solid rgba(0, 102, 255, 0.767);
-}
-</style>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-<body class="background-gradient min-h-screen">
-<div class="w-screen absolute top-0 left-0">
-            <img src="img/sunStar9.svg" alt="Sun Background" class="rotate-180 mx-auto opacity-20 object-cover w-2/3 h-full">
-        </div>
-    <div class="min-h-screen flex flex-col justify-between">
+<body class="background-gradient">
+    <div class="flex flex-col justify-between  min-h-screen">
+
     <div id="mobile-nav" class="block sm:hidden" x-data="{ open: false }">
         <div class="w-screen h-screen fixed top-0 left-0 z-30 bg-dark-gray opacity-60"
         x-bind:style="{ transform: open ? 'translateY(0)' : 'translateY(-100%)' }"
@@ -33,7 +26,7 @@
             <div class="text-5xl middle-light-color handjet-500 text-center mt-10" >quantum{L5D}</div>
             <nav >
                 <ul class="flex space-x-4 text-2xl handjet-500 flex-col flex gap-10 items-center">
-                    <li class="!mx-0"><a href="index.html" class="mx-0 middle-light-color curser-pointer hover:opacity-80 light-button-border p-3">Home</a></li>
+                    <li class="!mx-0"><a href="#" class="mx-0 middle-light-color curser-pointer hover:opacity-80 light-button-border p-3">My Experience</a></li>
                     <li class="!mx-0"><a href="#" class="mx-0 middle-light-color curser-pointer hover:opacity-80 light-button-border p-3">Let's Talk</a></li>
                     <li class="!mx-0"><a href="#" class="mx-0 middle-light-color curser-pointer hover:opacity-80 light-button-border p-3">Explore</a></li>
                 </ul>
@@ -54,60 +47,38 @@
         </div>
     </div>
     <div id="desktop-nav" class="sticky top-0 z-30 hidden sm:block">
+        <div>
         <header class="flex justify-between items-center max-w-screen-2xl mx-auto p-7">
             <div class="text-3xl font-bold dark-gray handjet-700">quantum{L5D}</div>
             <nav>
                 <ul class="flex space-x-4 2xl:text-2xl lg:text-xl sm:text-lg text-base handjet-700">
-                    <li><a href="index.html" class="dark-gray curser-pointer hover:opacity-80 button-border p-3">Home</a></li>
+                    <li><a href="#" class="dark-gray curser-pointer hover:opacity-80 button-border p-3">My Experience</a></li>
                     <li><a href="talk.php" class="dark-gray curser-pointer hover:opacity-80 button-border p-3">Let's Talk</a></li>
                     <li><a href="#" class="dark-gray curser-pointer hover:opacity-80 button-border p-3">Explore</a></li>
                 </ul>
             </nav>
         </header>
     </div>
-   <div id ="contactForm" class="h-full relative z-20 mt-14 sm:mt-0 mb-10 lg:mb-30">
- 
-           
-        <div class="max-w-screen-xl mx-auto xl:py-10 px-6 py-7 relative">
-        <h1 class="2xl:text-9xl lg:text-8xl sm:text-7xl leading-tight text-6xl text-center font-semibold text-dark playfair-display-500 more-neg-letterinline-block">Leave Me a Message</h1>
-        <h2 class="2xl:text-6xl lg:text-5xl text-4xl font-bold text-dark-gray-60 mb-4 playfair-display-500 text-center neg-letter">ask me anything or tell me anything</h2>
-
-    <form action="send_email.php" method="POST" class="mt-10 mx-auto w-2/3 flex flex-col space-y-4">
-        <div class="flex gap-4 w-full">
-        <label class="flex w-1/2 flex-col 2xl:2xl:text-xl sm:text-lg text-base sm:text-lg text-base leading-relaxed dark-gray opacity-80 lora-400">First Name
-        <input type="text" required name="fname" class="p-2 feature-border">
-        </label>
-        <label class="w-1/2 flex flex-col flex flex-col 2xl:2xl:text-xl sm:text-lg text-base sm:text-lg text-base leading-relaxed dark-gray opacity-80 lora-400">Last Name
-            <input type="text" required name="lname" class="feature-border p-2">
-        </label>
+    <div class="relative flex flex-col justify-center items-center">
+    <div class="w-screen absolute top-0 left-0">
+            <img src="img/sunStar1.svg" alt="Sun Background" class="rotate-180 mx-auto opacity-20 object-cover w-2/3 h-full">
         </div>
-        <label class="flex flex-col flex flex-col 2xl:2xl:text-xl sm:text-lg text-base sm:text-lg text-base leading-relaxed dark-gray opacity-80 mb-8 lora-400">Your Email Address
-        <input type="email" required name="email" class="p-2 feature-border">
-        </label>
-        <label class="flex flex-col flex flex-col 2xl:2xl:text-xl sm:text-lg text-base sm:text-lg text-base leading-relaxed dark-gray opacity-80 mb-8 lora-400">Your Message
-        <textarea spellcheck="true" name="message" required class="p-2 feature-border h-80"></textarea>
-        </label>
-        <div class="flex justify-end">
-        <button type="submit" class="dark-gray button-border handjet-700 2xl:text-3xl lg:text-xl hover:opacity-80 sm:text-lg text-base p-3">Send</button>
+        <div>
+        <h1 class="2xl:text-9xl lg:text-8xl sm:text-7xl leading-tight text-6xl font-semibold text-dark text-centerd mb-4 playfair-display-500">Thank you!</h1>
+        <p class="2xl:2xl:text-xl sm:text-lg text-base sm:text-lg text-base leading-relaxed dark-gray opacity-80 mb-12 lora-400 ">Your message has been sent successfully. We will get back to you soon.</p>
+        <a href="index.html" class="button-border handjet-700 text-3xl hover:opacity-80 p-3 dark-gray">Go Back to Home</a>
         </div>
-    </form>
         </div>
-
-</div>
+    </div>
 <div id="footer" class="h-full relative pb-2">
     <div class="absolute top-0 left-0 ">
         <img src="img/sunStar1.svg" alt="Sun Background" class="h-52 top-20 opacity-20">
 </div>
     <div class="flex justify-between h-full gap-2 flex-col max-w-screen-2xl mx-auto p-7">
-        
         <div class="text-3xl font-bold dark-gray handjet-700">quantum{L5D}</div>
             <p class="lora-400 opacity-80">I am also a developer. This website was created from scratch by me. I hope you enjoyed the experience it offered. <span class="lora-700">-Lyndsey</span></p>
             <p class="lora-400 opacity-80">If you would like to look at the repository in GitHub...<a target="_blank" class="hover-link underline" href="https://github.com/QuantumL5D/QuantumL5D">QuantumL5D REPO</a></p>
-                <div class="h-full mt-4 w-full flex justify-end mb-4">
-                    <a href="#top" class="dark-gray curser-pointer hover:opacity-80 handjet-700 button-border p-3">
-                        Back to Top
-                    </a>
-                </div>
+               
     </div>
 </div>
 </div>
@@ -120,7 +91,5 @@ icons.forEach (icon => {
 });
 
 </script>
-</body>
-
 </body>
 </html>
